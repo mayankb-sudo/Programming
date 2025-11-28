ae8ba84f3c4506d7ca0651efb9b987320b56f3c9
Mayank Bhardwaj <learn2earn.study@gmail.com>
2025-11-28 12:21:03 +0530

    Pattern 21

---
+++ Patterns/patern21.py
@@ -0,0 +1,17 @@
+# Pattern - 21: Hollow Rectangle Pattern
+
+
+
+n=5
+
+for i in range(n):
+            # Inner loop for columns
+    for j in range(n):
+        # Print star if it's a border cell
+        if i == 0 or j == 0 or i == n - 1 or j == n - 1:
+            print("*", end="")
+        # Print space otherwise
+        else:
+            print(" ", end="")
+    # Move to next line after each row
+    print()
